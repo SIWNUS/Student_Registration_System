1d87cfd7c2eacd84c4e5f42f0445afd9862ee7c1
SIWNUS <suswinpalaniswamy@gmail.com>
2025-03-08 15:11:21 +0530

    updated as of 08-03-2025 for publishing

--- config/db.php
+++ config/db.php
@@ -11,17 +11,4 @@ if ($conn->connect_error) {
     die("Error connecting to database: \n" . $conn->connect_error);
 }
 
-$sql = "CREATE TABLE IF NOT EXISTS students (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(155) NOT NULL,
-    age VARCHAR(5) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone VARCHAR(20) NOT NULL,
-    address TEXT NOT NULL,
-    department VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);";
-
-$conn->query($sql);
-
 ?>
